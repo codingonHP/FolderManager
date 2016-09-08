8a2d14f4fb095b08799e818cea1106329ae24695
vishal anand <vishal_24_anand@hotmail.com>
2016-09-09 01:35:23 +0530

    Moves file from the deepest folder to one level above

--- FolderManager/MainWindow.xaml.cs
+++ FolderManager/MainWindow.xaml.cs
@@ -1,17 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.Windows;
+using System.Windows.Forms;
+using System.IO;
 using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace FolderManager
 {
@@ -23,6 +13,65 @@ namespace FolderManager
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void btnBrowse_Click(object sender, RoutedEventArgs e)
+        {
+            FolderBrowserDialog openFileDialog = new FolderBrowserDialog();
+            var openFolderBrowserDialogResult = openFileDialog.ShowDialog();
+            if (openFolderBrowserDialogResult == System.Windows.Forms.DialogResult.OK)
+            {
+                btnBrowse.IsEnabled = false;
+                prgBar.IsIndeterminate = true;
+                var selectedPath = openFileDialog.SelectedPath;
+
+                Task task = new Task(() =>
+                {
+                    RecursiveTraversal(selectedPath);
+                });
+
+                task.Start();
+
+                task.ContinueWith(t =>
+                {
+                    Dispatcher.Invoke(() =>
+                    {
+                        btnBrowse.IsEnabled = true;
+                    });
+                    System.Windows.MessageBox.Show("Done moving files");
+                });
+            }
+        }
+
+        private void RecursiveTraversal(string path)
+        {
+            var childDirs = Directory.GetDirectories(path);
+            if (childDirs.Length == 0)
+            {
+                var files = Directory.GetFiles(path);
+
+                foreach (var file in files)
+                {
+                    var destination = Directory.GetParent(path).FullName + "\\" + Path.GetFileName(file);
+                    if (!File.Exists(destination))
+                    {
+                        File.Move(file, destination);
+                    }
+                }
+                if (Directory.GetFiles(path).Length == 0)
+                {
+                    Directory.Delete(path);
+                }
+            }
+            else
+            {
+                foreach (var dir in childDirs)
+                {
+                    RecursiveTraversal(dir);
+                }
+            }
+
+
         }
     }
 }
