64681f1665a0781a03f41311bb2541e7109391f0
vishal anand <vishal_24_anand@hotmail.com>
2016-09-10 15:07:55 +0530

    modified to copy all the files up one folder level and automatically name name them by index

--- FolderManager/MainWindow.xaml.cs
+++ FolderManager/MainWindow.xaml.cs
@@ -2,6 +2,10 @@
 using System.Windows.Forms;
 using System.IO;
 using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Linq;
+using System.Collections.Generic;
+using System;
 
 namespace FolderManager
 {
@@ -10,9 +14,12 @@ namespace FolderManager
     /// </summary>
     public partial class MainWindow : Window
     {
+        private IComparer<string> comparer;
+
         public MainWindow()
         {
             InitializeComponent();
+            comparer = new StringComparer();
         }
 
         private void btnBrowse_Click(object sender, RoutedEventArgs e)
@@ -23,36 +30,55 @@ namespace FolderManager
             {
                 btnBrowse.IsEnabled = false;
                 prgBar.IsIndeterminate = true;
+                prgBar.IsEnabled = true;
+
                 var selectedPath = openFileDialog.SelectedPath;
 
                 Task task = new Task(() =>
                 {
-                    RecursiveTraversal(selectedPath);
+                    RecursiveTraversal(selectedPath,0);
                 });
+
+                Stopwatch sw = new Stopwatch();
 
                 task.Start();
 
+                sw.Start();
                 task.ContinueWith(t =>
                 {
+                    sw.Stop();
+
                     Dispatcher.Invoke(() =>
                     {
                         btnBrowse.IsEnabled = true;
+                        prgBar.IsIndeterminate = false;
+                        prgBar.IsEnabled = false;
+
                     });
-                    System.Windows.MessageBox.Show("Done moving files");
+                    System.Windows.MessageBox.Show($"Done moving files in {sw.Elapsed.TotalHours} hours {sw.Elapsed.TotalMinutes} minutes {sw.Elapsed.TotalSeconds} secs {sw.Elapsed.TotalMilliseconds} ms"
+                                                    , "Process done"
+                                                    , MessageBoxButton.OK
+                                                    , MessageBoxImage.Information
+                                                    , MessageBoxResult.OK
+                                                    , System.Windows.MessageBoxOptions.ServiceNotification);
                 });
             }
         }
 
-        private void RecursiveTraversal(string path)
+        private int RecursiveTraversal(string path, int fileIndex)
         {
+            int step = 0;
             var childDirs = Directory.GetDirectories(path);
+            childDirs = childDirs.OrderBy(d => d, comparer).ToArray();
             if (childDirs.Length == 0)
             {
                 var files = Directory.GetFiles(path);
-
+                int index = 0;
+                int maxIndex = 0;
                 foreach (var file in files)
                 {
-                    var destination = Directory.GetParent(path).FullName + "\\" + Path.GetFileName(file);
+                    maxIndex = fileIndex + index++;
+                    var destination = Directory.GetParent(path).FullName + "\\" + maxIndex + Path.GetExtension(file);
                     if (!File.Exists(destination))
                     {
                         File.Move(file, destination);
@@ -62,16 +88,37 @@ namespace FolderManager
                 {
                     Directory.Delete(path);
                 }
+
+                return ++maxIndex;
             }
             else
             {
                 foreach (var dir in childDirs)
                 {
-                    RecursiveTraversal(dir);
+                    var maxIndex = RecursiveTraversal(dir,step);
+                    step = maxIndex;
                 }
             }
 
+            return step;
+        }
 
+        private class StringComparer : IComparer<string>
+        {
+            public int Compare(string x, string y)
+            {
+                var xDirName = x.Split('\\').Last();
+                var yDirName = y.Split('\\').Last();
+
+                int xresult;
+                int yresult;
+
+                int.TryParse(xDirName, out xresult);
+                int.TryParse(yDirName, out yresult);
+
+                return xresult - yresult;
+
+            }
         }
     }
 }
